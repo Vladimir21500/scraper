8ffff9b1604feaf78d970f2746285d37903d4576
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2023-08-03 20:05:11 +0300

    fix: add browser revision

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -6,4 +6,5 @@ const { join } = require("path");
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  browserRevision: "117.0.5897.0",
 };
