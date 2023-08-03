28cd197611e58ee08e48d0d1068911dbba120d61
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2023-08-03 20:34:08 +0300

    fix: update version

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -6,5 +6,5 @@ const { join } = require("path");
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-  browserRevision: "117.0.5926.0",
+  browserRevision: "115.0.5790.102",
 };
