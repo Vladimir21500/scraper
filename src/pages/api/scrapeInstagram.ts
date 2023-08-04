8d33166fac2feecd72b0e3932b22303951a5f02f
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2023-08-04 14:05:08 +0300

    fix: use testingBot service

--- src/pages/api/scrapeInstagram.ts
+++ src/pages/api/scrapeInstagram.ts
@@ -1,14 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import puppeteer from "puppeteer";
 
+const capabilities = {
+  "tb:options": {
+    key: "39f7b0fcbe91d6768e67f730dcec0c74",
+    secret: "569c082858cb8a7cd5c8ae72ba720b67",
+  },
+  browserName: "chrome",
+  browserVersion: "latest",
+};
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const url = req.query.url as string;
 
   console.log(url);
 
   try {
-    const browser = await puppeteer.launch({
-      headless: "new",
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: `wss://cloud.testingbot.com/puppeteer?capabilities=${encodeURIComponent(
+        JSON.stringify(capabilities)
+      )}`,
     });
     const page = await browser.newPage();
     await page.goto(url);
