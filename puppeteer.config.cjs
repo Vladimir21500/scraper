a3b478e7304debcaa067f3c180662bdf5cbc37a0
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2023-08-07 13:03:58 +0300

    fix: use last stable version

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -6,5 +6,5 @@ const { join } = require("path");
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-  browserRevision: "115.0.5790.102",
+  browserRevision: "115.0.5790.170",
 };
